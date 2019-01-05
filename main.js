b5455bf012442ad1e0faaecd2b1b7d892d725045
Andres Suarez <andresfelipe9619@gmail.com>
2019-01-05 10:04:28 -0500

    Add search control and autocomplete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const TILE_LAYER =
-"https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
 const TOKEN =
-"pk.eyJ1IjoiYW5kcmVzOTYxOSIsImEiOiJjanExdTFodjMwYXQyNDNuMmVvazV6eHBlIn0.kOpHKEx5EBGD8YIXmKRQWA";
+  "pk.eyJ1IjoiYW5kcmVzOTYxOSIsImEiOiJjanExdTFodjMwYXQyNDNuMmVvazV6eHBlIn0.kOpHKEx5EBGD8YIXmKRQWA";
 const URL = "https://www.todosurf.com/dev/config/classes/geojson.php";
 
 const BOUNDS = new L.LatLngBounds(
@@ -18,39 +18,49 @@ const MAP_OPTIONS = {
 let mMap = null;
 let mFeatures = [];
 let categories = {};
-let category = "";
 let baseLayer = null;
 
 $(document).ready(() => {
   mMap = L.map("mapid", MAP_OPTIONS);
   baseLayer = L.tileLayer(TILE_LAYER, {
     maxZoom: 18,
-    id: "mapbox.satellite",
+    id: "mapbox.light",
     accessToken: TOKEN
   });
 
   baseLayer.addTo(mMap);
   loadSurfingFeatures();
-
 });
 
 const loadSurfingFeatures = async () => {
-  // var result = await fetch(URL);
-  // var data = await result.json();
-  var data = GEO_JSON;
-  var features = new L.GeoJSON(data.features, {
+  // let result = await fetch(URL);
+  // let data = await result.json();
+  let data = GEO_JSON;
+  let features = new L.GeoJSON(data.features, {
     pointToLayer,
     onEachFeature
   });
 
   mFeatures = features;
   // mMap.addLayer(mFeatures);
-  // loadSearchControl()
+  loadSearchControl();
   loadGroupedLayers();
 };
 
 const onEachFeature = (feature, layer) => {
-  category = feature.properties.prioridad;
+  let category = feature.properties.prioridad;
+  let source = feature
+  $("#searchboxinput").autocomplete({
+    source: [
+      "c++",
+      "java",
+      "php",
+      "coldfusion",
+      "javascript",
+      "asp",
+      "ruby"
+    ]
+  });
   if (typeof categories[category] === "undefined") {
     categories[category] = [];
   }
@@ -58,14 +68,12 @@ const onEachFeature = (feature, layer) => {
 };
 
 const pointToLayer = (feature, latlng) => {
-  let text = `<div ><a target="_blank" href="${
-    feature.properties.enlace
-  }" > 
+  let text = `<div ><a target="_blank" href="${feature.properties.enlace}" > 
   ${feature.properties.nombre} 
   </a></div>`;
   let mIcon = L.divIcon({
     iconSize: [8, 8],
-    iconAnchor: [5, 5],
+    iconAnchor: [4, 4],
     html: ""
   });
   return L.marker(latlng, {
@@ -74,7 +82,7 @@ const pointToLayer = (feature, latlng) => {
 };
 
 const loadGroupedLayers = () => {
-  var overlaysObj = {},
+  let overlaysObj = {},
     categoryName,
     categoryArray,
     categoryLG;
@@ -88,23 +96,65 @@ const loadGroupedLayers = () => {
   // mMap.addLayer(overlaysObj["0"]);
   mMap.addLayer(overlaysObj["1"]);
 
-  var groupedOverlays = {
+  let groupedOverlays = {
     Prioridad: {
       "1": overlaysObj["1"]
       // "0": overlaysObj["0"]
     }
   };
-  var mapabase = {
+  let mapabase = {
     "Capa base": baseLayer
   };
   L.control.groupedLayers(mapabase, groupedOverlays).addTo(mMap);
 };
 
-const getRandomColor = () => {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+const loadSearchControl = () => {
+  let SearchBox = L.Control.extend({
+    options: { position: "topleft" },
+    onAdd: () => {
+      let container = L.DomUtil.create("div");
+      container.id = "controlcontainer";
+      $(container).html(getControlHtmlContent());
+      setTimeout(() => {
+
+        $("#searchbox-searchbutton").click(handleOnSearch());
+      }, 1);
+      L.DomEvent.disableClickPropagation(container);
+      return container;
+    }
+  });
+  new SearchBox().addTo(mMap);
+};
+
+
+const handleOnSearch = event => {
+  let inputvalue = $("#searchboxinput").val()
+
+};
+const clearMap = () => {
+  mMap.eachLayer(layer => {
+    if (layer instanceof L.GeoJSON) {
+      //Do marker specific actions here
+
+      mMap.removeLayer(layer);
+    }
+    console.log(layer);
+  });
+};
+
+const getControlHtmlContent = () => {
+  return `
+  <div id="controlbox">
+  <div id="boxcontainer" class="searchbox searchbox-shadow">
+    <input id="searchboxinput" type="text" style="position: relative;" />
+  </div>
+  <div class="searchbox-searchbutton-container">
+    <button
+      aria-label="search"
+      id="searchbox-searchbutton"
+      class="searchbox-searchbutton"
+    ></button>
+    <span aria-hidden="true" style="display:none;">search</span>
+  </div>
+</div>`;
 };
